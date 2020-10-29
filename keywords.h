04f77c97fd6db0a1b8cd402e640210562b1cb5cd
Alpatron <1508117+alpatron@users.noreply.github.com>
2020-10-29 16:47:53 +0100

    Add file header

--- keywords.h
+++ keywords.h
@@ -1,3 +1,19 @@
+/*File name: keywords.h ---------------------------------------------*
+ |Project:    Implementace překladače imperativního jazyka IFJ20     |
+ |Team:       124, varianta II                                       |
+ |Authors:    Ondřej Sloup (xsloup02)                                |
+ |            Viktor Rucký (xrucky01)                                |
+ |                                                                   |
+ |  _      _     _   __                   __  _        _             |
+ | | |    (_)   | | /_/                  /_/ | |      | |            |
+ | | |     _  __| | ___   _   _  __   ___   _| |_ __ _| |__  _   _   |
+ | | |    | |/ _` |/ _ \ | | | | \ \ / / | | | __/ _` | '_ \| | | |  |
+ | | |____| | (_| |  __/ | |_| |  \ V /| |_| | || (_| | | | | |_| |  |
+ | |______|_|\__,_|\___|  \__,_|   \_/  \__, |\__\__,_|_| |_|\__,_|  |
+ |                                       __/ |                       |
+ |                                      |___/                        |
+ *-------------------------------------------------------------------*/
+
 #include <token.h>
 
 typedef struct {
@@ -30,5 +46,5 @@ const keywordEntry keywords[] = {
     },
     {
     "string", {TokenDataType, {.t = TypeString}}
-    }    
+    }
 };
