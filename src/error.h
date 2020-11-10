db3011d4aecf7089ab1c0731d1f99bf87737ba68
Alpatron <1508117+alpatron@users.noreply.github.com>
2020-11-11 00:48:32 +0100

    Backpropagate changes to error.h

--- src/error.h
+++ src/error.h
@@ -1,3 +1,6 @@
+#ifndef ERROR_H
+#define ERROR_H
+
 #define LEXICAL_ERROR 1
 #define SYNTAX_ERROR 2
 #define SEMANTIC_ERROR_DEFINITION 3
@@ -7,3 +10,7 @@
 #define SEMANTIC_ERROR_OTHER 7
 #define SEMANTIC_ERROR_DIV_ZERO 9
 #define INTERNAL_ERROR 99
+
+typedef int make_iso_compilers_happy;
+
+#endif
