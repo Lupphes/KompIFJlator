883ce8467537598860d813609e87af1aa16d2063
Alpatron <1508117+alpatron@users.noreply.github.com>
2020-11-20 20:37:33 +0100

    Add symtable specification

---
+++ src/symbol.h
@@ -0,0 +1,41 @@
+/*File name: symbol.h -----------------------------------------------*
+ |Project:    Implementace překladače imperativního jazyka IFJ20     |
+ |Team:       124, varianta II                                       |
+ |Authors:    Viktor Rucký (xrucky01)                                |
+ |                                                                   |
+ |  _      _     _   __                   __  _        _             |
+ | | |    (_)   | | /_/                  /_/ | |      | |            |
+ | | |     _  __| | ___   _   _  __   ___   _| |_ __ _| |__  _   _   |
+ | | |    | |/ _` |/ _ \ | | | | \ \ / / | | | __/ _` | '_ \| | | |  |
+ | | |____| | (_| |  __/ | |_| |  \ V /| |_| | || (_| | | | | |_| |  |
+ | |______|_|\__,_|\___|  \__,_|   \_/  \__, |\__\__,_|_| |_|\__,_|  |
+ |                                       __/ |                       |
+ |                                      |___/                        |
+ *-------------------------------------------------------------------*/
+
+#ifndef SYMBOL_H
+#define SYMBOL_H
+#include "str.h"
+#include "data_type.h"
+
+typedef struct {
+    string id;
+    struct {
+        struct {
+            string id;
+            DataType type; 
+        } param[];
+        int count;
+    } parameters;
+    struct {
+        DataType type[];
+        int count;
+    } returnTypes;
+} SymbolFunction;
+
+typedef struct {
+    string id;
+    DataType type;
+} SymbolVariable;
+
+#endif
