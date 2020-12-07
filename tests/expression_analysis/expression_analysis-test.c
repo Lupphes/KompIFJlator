1f4de9d5e898996ec763f46b5ecc969c1ab739b9
Luppo <ondrej.sloup@protonmail.com>
2020-12-07 23:26:51 +0100

    Add support for +=,-=,*=,/=

--- tests/expression_analysis/expression_analysis-test.c
+++ tests/expression_analysis/expression_analysis-test.c
@@ -3,17 +3,20 @@
 #include "expression_analysis-test.h"
 #include "./../../src/parser_common.h"
 #include "./../../src/symtable.h"
+#include "./../../src/parser.h"
 
 #include <stdio.h>
 
 int main() {
     int returnCode = 0;
     ExpExp *expression;
-    Operator assingmentOperation = PLAIN_ASSIGEMENT;
-    SymbolVariable *symbol = NULL;
+    OperatorAssign assingmentOperation = 1;
+    string ahoj = {.allocSize = 2,.length = 2,.str = "ah"};
+    SymbolVariable symbol = {.id = ahoj,.type = TypeInt,.uid = 1505};
+    SymbolVariable *haha = &symbol;
     initVariableTableStack();
     acceptAny();
-    int result = parseExpression(&expression, assingmentOperation, symbol);
+    int result = parseExpression(&expression, assingmentOperation, haha);
     printf("Return code is: %d\n", result);
     return result;
 }
